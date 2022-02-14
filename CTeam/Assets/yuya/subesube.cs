30cf949c97d2837d4b05f0f66e1004c9d5c1cc0c
KBC12006094\kbc12006094 <yuuya-kuniyoshi-20h@stu.kbc.ac.jp>
2022-02-14 14:41:21 +0900

    ワンクリックするとオブジェクトが滑り出す

---
+++ CTeam/Assets/yuya/subesube.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class subesube : MonoBehaviour
+{
+    private Rigidbody rb;
+    private BoxCollider collider;
+    public PhysicMaterial slip;
+    public PhysicMaterial nonslip;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        collider = GetComponent<BoxCollider>();
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Object")
+        {
+            collider.material = slip;
+        }
+    }
+}
