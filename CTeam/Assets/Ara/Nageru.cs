93dc82727527109f9ed4e1acc7af2597067acb54
KBC12006118\kbc12006118 <koudai-arashiro-20h@stu.kbc.ac.jp>
2022-02-16 11:28:49 +0900

    手にボールを持つようにした

---
+++ CTeam/Assets/Ara/Nageru.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nageru : MonoBehaviour
+{
+    public Transform oya;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        oya = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            if(Input.GetKeyDown(KeyCode.F))
+            {
+                this.transform.parent = GameObject.Find("WeponBoal").transform;
+
+                this.transform.localPosition = new Vector3(0.09f, -0.06f, 0f);
+            }
+        }
+    }
+}
