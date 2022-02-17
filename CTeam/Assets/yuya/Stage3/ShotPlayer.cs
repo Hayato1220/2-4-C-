9731b380dd0dc3e3e83307390213b70e66c85a2a
KBC12006094\kbc12006094 <yuuya-kuniyoshi-20h@stu.kbc.ac.jp>
2022-02-17 11:43:07 +0900

    a

---
+++ CTeam/Assets/yuya/Stage3/ShotPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotPlayer : MonoBehaviour
+{
+    public GameObject shotObject;
+    public float force = 10.0f;
+    public Rigidbody rb;
+
+
+    void Start()
+    {
+        rb = shotObject.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("B"))
+        {
+            Instantiate(shotObject, transform.position + transform.forward + transform.up, transform.rotation);
+            //Instantiate(shotObject, transform.position + transform.up / 1000, transform.rotation);
+        }
+    }
+}
