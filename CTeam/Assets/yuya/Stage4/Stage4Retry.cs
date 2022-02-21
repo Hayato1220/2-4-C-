3eaa203cae86d8486ba0ead76c8b11e2773086ac
KBC12006094\kbc12006094 <yuuya-kuniyoshi-20h@stu.kbc.ac.jp>
2022-02-21 14:57:59 +0900

    ふわふわ用のステージ(yuyaStage4)追加、ふわふわの処理はまだないです。

---
+++ CTeam/Assets/yuya/Stage4/Stage4Retry.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Stage4Retry : MonoBehaviour
+{
+    public void OnClickStartButton()
+    {
+        SceneManager.LoadScene("yuyaStage4");
+    }
+}
