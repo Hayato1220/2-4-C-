f336e67d9ee0705665697ad9205763dbedec61f4
KBC12006094\kbc12006094 <yuuya-kuniyoshi-20h@stu.kbc.ac.jp>
2022-02-22 15:01:00 +0900

    ステージ複製しただけ

---
+++ CTeam/Assets/yuya/Stage5/clone.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clone : MonoBehaviour
+{
+    //オブジェクトを生成する元となるPrefabへの参照を保持します。
+    public GameObject prefabObj;
+
+
+    //生成したオブジェクトの親オブジェクトへの参照を保持します。
+    public Transform parentTran;
+
+
+    //マテリアルを保持します。
+    public Material yellow;
+    public Material Red;
+    public Material blue;
+
+
+    //Prefabを生成する高さを定義します。
+    public float heigtht;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //フレームカウントが10の倍数の時にオブジェクトを生成します。
+        if(Time.frameCount % 10 == 0)
+        {
+            CreateObject();
+        }
+    }
+
+
+    //Prefabからオブジェクトを生成します。
+    void CreateObject()
+    {
+        //ゲームオブジェクトを生成します
+        GameObject obj = Instantiate(prefabObj, Vector3.zero, Quaternion.identity);
+
+
+        //ゲームオブジェクトの親オブジェクトを設定します。
+        obj.transform.SetParent(parentTran);
+
+        //ゲームオブジェクトの位置を設定します。
+        obj.transform.localPosition = new Vector3(0f, height, 0f);
+
+        //マテリアルをランダムで設定します。
+        int materialId = Random.Range(0, 3);
+    }
+}
