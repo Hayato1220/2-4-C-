7f85e3657bbe5d7a530a71beaa8a12291c38b8e7
KBC12006094\kbc12006094 <yuuya-kuniyoshi-20h@stu.kbc.ac.jp>
2022-02-22 13:11:34 +0900

    何もしてない

---
+++ CTeam/Assets/yuya/Stage5/Slice.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EzySlice;
+
+public class Slice : MonoBehaviour
+{
+    public GameObject Cube;
+
+    private void Start()
+    {
+        Cube.SliceInstantiate
+            (
+                position: Vector3.zero,
+                direction: new Vector3(1, 1, 1)
+            ); ;
+    }
+
+}
