a611479ca1d310f815ddf9f97d395ebae7088024
KBC12006094\kbc12006094 <yuuya-kuniyoshi-20h@stu.kbc.ac.jp>
2022-02-24 12:17:21 +0900

    バラバラの処理難しいね

---
+++ CTeam/Assets/yuya/Stage5/DestroyWall.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyWall : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Object"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
--- CTeam/Assets/yuya/Stage5/clone.cs
+++ CTeam/Assets/yuya/Stage5/clone.cs
@@ -1,55 +1,73 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class clone : MonoBehaviour
-{
-    //オブジェクトを生成する元となるPrefabへの参照を保持します。
-    public GameObject prefabObj;
+//public class clone : MonoBehaviour
+//{
+//    //オブジェクトを生成する元となるPrefabへの参照を保持します。
+//    public GameObject prefabObj;
 
+//    public GameObject ParentObj;
 
-    //生成したオブジェクトの親オブジェクトへの参照を保持します。
-    public Transform parentTran;
-
-
-    //マテリアルを保持します。
-    public Material yellow;
-    public Material Red;
-    public Material blue;
+//    //生成したオブジェクトの親オブジェクトへの参照を保持します。
+//    public Transform parentTran;
 
 
-    //Prefabを生成する高さを定義します。
-    public float heigtht;
+//    //マテリアルを保持します。
+//    public Material yellow;
+//    public Material Red;
+//    public Material blue;
 
-    void Start()
-    {
+//    //Prefabを生成する高さを定義します。
+//    public float height = 1.5f;
 
-    }
+//    void Start()
+//    {
 
-    void Update()
-    {
-        //フレームカウントが10の倍数の時にオブジェクトを生成します。
-        if(Time.frameCount % 10 == 0)
-        {
-            CreateObject();
-        }
-    }
+//    }
+
+//    void Update()
+//    {
+//        Transform parentObj = ParentObj.transform; //targetObjectに変更
+//    }
+
+//    void OnCollisionStay(Collision other)
+//    {
+//        if(other.gameObject.tag == "Object")
+//        {
+//            if (Input.GetButton("B"))
+//            {
+//                CreateObject();
+
+//            }
+
+//        }
+//    }
 
 
-    //Prefabからオブジェクトを生成します。
-    void CreateObject()
-    {
-        //ゲームオブジェクトを生成します
-        GameObject obj = Instantiate(prefabObj, Vector3.zero, Quaternion.identity);
+//    //Prefabからオブジェクトを生成します。
+//    void CreateObject()
+//    {
+
+//        //ゲームオブジェクトを生成します
+//        GameObject obj = Instantiate(prefabObj, Vector3.zero, Quaternion.identity);
+
+//        ////ゲームオブジェクトの親オブジェクトを設定します。
+//        //obj.transform.SetParent(parentTran);
+
+//        //ゲームオブジェクトの位置を設定します。
+//        obj.transform.position = parentObj.position;
 
 
-        //ゲームオブジェクトの親オブジェクトを設定します。
-        obj.transform.SetParent(parentTran);
+//        //ゲームオブジェクトの親オブジェクトを設定します。
+//        //obj.transform.SetParent(parentTran);
 
-        //ゲームオブジェクトの位置を設定します。
-        obj.transform.localPosition = new Vector3(0f, height, 0f);
+//        //ゲームオブジェクトの位置を設定します。
+//        //obj.transform.position = new Vector3(0f, height, 0f);
 
-        //マテリアルをランダムで設定します。
-        int materialId = Random.Range(0, 3);
-    }
-}
+//        //マテリアルをランダムで設定します。
+//        //int materialId = Random.Range(0, 3);
+
+//        //Destroy(OriginObj, 3f);
+//    }
+//}
