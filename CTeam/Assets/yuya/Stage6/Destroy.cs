2560627cf55938334f51138a176127672e351bc0
KBC12006094\kbc12006094 <yuuya-kuniyoshi-20h@stu.kbc.ac.jp>
2022-03-01 15:01:18 +0900

    壁にオブジェクトがが当たると消えるようにした

---
+++ CTeam/Assets/yuya/Stage6/Destroy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    public GameObject DestroyWall;
+
+    float getCubeSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        getCubeSpeed = Speed.CubeSpeed();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Wall")
+        {
+            if(getCubeSpeed >= 10.0f)
+            {
+                Destroy(DestroyWall);
+
+                Destroy(this.gameObject);
+            }
+
+        }
+    }
+}
--- CTeam/Assets/yuya/Stage6/Speed.cs
+++ CTeam/Assets/yuya/Stage6/Speed.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Speed : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //1フレーム前の位置
+    private Vector3 _prevPosition;
+
+    private static float speed;
+
+
+    private void Start()
     {
-        
+        //初期位置を保持
+        _prevPosition = transform.position;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //deltaTimeが0の場合は何もしない
+        if (Mathf.Approximately(Time.deltaTime, 0))
+        {
+            return;
+        }
+
+        //現在位置取得
+        var position = transform.position;
+
+        //現在速度計算
+        var speed = (position - _prevPosition) / Time.deltaTime;
+
+
+        //現在速度をログ出力
+        print($"velocity = {speed}");
+
+        Debug.Log(speed);
+
+        //前フレーム位置を取得
+        _prevPosition = position;
+    }
+
+    public static float CubeSpeed()
+    {
+        return speed;
     }
 }
