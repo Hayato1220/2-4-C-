4ca5916943500d26f6667e681aa67ce26ece160f
KBC12006094\kbc12006094 <yuuya-kuniyoshi-20h@stu.kbc.ac.jp>
2022-03-03 16:09:29 +0900

    滑らせるやつ

---
+++ CTeam/Assets/yuya/Stage6/Slip.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slip : MonoBehaviour
+{
+    public PhysicMaterial slip;
+
+    private BoxCollider CubeCollider;
+
+    void Start()
+    {
+        CubeCollider = GetComponent<BoxCollider>();
+    }
+
+    void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (Input.GetButton("B"))
+            {
+                CubeCollider.material = slip;
+            }
+        }
+    }
+}
