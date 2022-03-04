d2bc5fb9f7f5593d755b8f54f3b1a36671635790
KBC12006118\kbc12006118 <koudai-arashiro-20h@stu.kbc.ac.jp>
2022-03-04 10:19:00 +0900

    arascene編集

---
+++ CTeam/Assets/Ara/moyouBlue.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moyouBlue : MonoBehaviour
+{
+    private static bool dousyoku;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dousyoku = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Blue")
+        {
+            dousyoku = true;
+        }
+    }
+
+    public static bool Blue()
+    {
+        return dousyoku;
+    }
+}
