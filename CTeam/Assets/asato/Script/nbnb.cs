939dace15a0111e5b4ee0cd73fe0f857236adf69
MSI\あ <using System.Collections>
2022-03-16 14:05:30 +0900

    ねばねば追加

---
+++ CTeam/Assets/asato/Script/nbnb.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nbnb : MonoBehaviour
+{
+
+    //変数を作る
+    Rigidbody rb;
+
+    //ゲーム起動時に呼び出される
+    void Start()
+    {
+        //Rigidbodyを取得
+        rb = GetComponent<Rigidbody>();
+
+        //動かす
+        //rb.AddForce(10, 0, 0);
+
+    }
+
+    //毎フレーム(いっぱい！)ごと呼ばれる
+    void Update()
+    {
+        //もしスペースキーが押されたら
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //Rigidbodyを停止
+            rb.velocity = Vector3.zero;
+        }
+    }
+}
