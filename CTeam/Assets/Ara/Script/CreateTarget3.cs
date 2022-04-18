fa8a6470231f5af17ef02f0de047b56aa42224e2
KBC12006118\kbc12006118 <koudai-arashiro-20h@stu.kbc.ac.jp>
2022-04-18 09:29:25 +0900

    ステージ制作

---
+++ CTeam/Assets/Ara/Script/CreateTarget3.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateTarget3 : MonoBehaviour
+{
+    public GameObject batu;
+    public GameObject maru;
+    public GameObject cube;
+
+    public static bool target3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target3 = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Object")
+        {
+            batu.SetActive(false);
+            maru.SetActive(true);
+            Destroy(cube);
+
+            target3 = true;
+        }
+    }
+
+    public static bool GetTarget3()
+    {
+        return target3;
+    }
+}
---
+++ CTeam/Assets/asato/Script/MoveTarget.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTarget : MonoBehaviour
+{
+    public bool getAll;
+
+    Vector3 targetpos;
+
+    [SerializeField]
+    public float Speed = 0.05f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetpos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        getAll = TobiraBreak.Move();
+
+        if(getAll == true)
+        {
+            if (targetpos.y <= 8.5f)
+            {
+                targetpos.y += Speed;
+                transform.position = new Vector3(targetpos.x, targetpos.y, targetpos.z);
+            }
+
+            if(targetpos.y >= 8.4f && targetpos.z <= 3.4f)
+            {
+                targetpos.z += Speed;
+                transform.position = new Vector3(targetpos.x, targetpos.y, targetpos.z);
+            }
+        }
+    }
+}
