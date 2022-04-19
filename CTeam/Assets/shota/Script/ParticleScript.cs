d43df93563191a3052504055dd85e8de04d4973b
shotakinjou <hakumaimk-777>
2022-04-19 11:21:29 +0900

    no message

--- CTeam/Assets/shota/Script/ParticleScript.cs
+++ CTeam/Assets/shota/Script/ParticleScript.cs
@@ -6,9 +6,9 @@ public class ParticleScript : MonoBehaviour
 {
     public GameObject particleObject;
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionStay(Collision collision)
     {
-        if (collision.gameObject.tag == "Object") //Objectタグの付いたゲームオブジェクトと衝突したか判別
+        if (collision.gameObject.tag == "Object") //Objectタグの付いたゲームオブジェクトとふれている間判別
         {
             Instantiate(particleObject, this.transform.position, Quaternion.identity); //パーティクル用ゲームオブジェクト生成
         }
