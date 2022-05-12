9cf5d0e1faf2e98e761a6307df97bced5be70cfd
shotakinjou <hakumaimk-777>
2022-05-12 09:21:39 +0900

    no message

--- CTeam/Assets/shota/Script/neba.cs
+++ CTeam/Assets/shota/Script/neba.cs
@@ -6,12 +6,9 @@ public class neba : MonoBehaviour
 {
     private GameObject neba_P;
 
-    private GameObject childObjneba;
     void Start()
     {
         neba_P = Resoursces.Load("NebaEffect") as GameObject;
-        childObjneba = (GameObject)Instantiate(neba_P, this.transform.position, Quaternion.identity);
-        childObjneba.transform.parent = this.gameObject.transform;
     }
 
     void Update()
