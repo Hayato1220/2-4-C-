07c3fe6ab33e04abe355f6d3ec25b9b04114e863
shotakinjou <hakumaimk-777>
2022-05-18 13:45:48 +0900

    no message

--- CTeam/Assets/shota/Script/FrontEffect.cs
+++ CTeam/Assets/shota/Script/FrontEffect.cs
@@ -1,28 +1,28 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿//using UnityEngine;
+//using System.Collections;
 
-public class FrontEffect: MonoBehaviour
-{
-    //エフェクト用のレイヤー名
-    private string EFFECT_SORTING_LAYER_NAME = "Effect";
+//public class FrontEffect: MonoBehaviour
+//{
+//    //エフェクト用のレイヤー名
+//    private string EFFECT_SORTING_LAYER_NAME = "Effect";
 
-    private void Awake()
-    {
-        SetSortingLayer(transform);
-    }
+//    private void Awake()
+//    {
+//        SetSortingLayer(transform);
+//    }
 
-    private void SetSortingLayer(Transform parent)
-    {
-        //レンダラーがある場合のみレイヤーを設定
-        if (parent.gameObject.renderer)
-        {
-            parent.gameObject.renderer.sortingLayerName = EFFECT_SORTING_LAYER_NAME;
-        }
+//    private void SetSortingLayer(Transform parent)
+//    {
+//        //レンダラーがある場合のみレイヤーを設定
+//        if (parent.gameObject.renderer)
+//        {
+//            parent.gameObject.renderer.sortingLayerName = EFFECT_SORTING_LAYER_NAME;
+//        }
 
-        //子がいる場合には、それにも同じ処理を行う
-        foreach (Transform child in parent.transform)
-        {
-            SetSortingLayer(child);
-        }
-    }
-}
+//        //子がいる場合には、それにも同じ処理を行う
+//        foreach (Transform child in parent.transform)
+//        {
+//            SetSortingLayer(child);
+//        }
+//    }
+//}
