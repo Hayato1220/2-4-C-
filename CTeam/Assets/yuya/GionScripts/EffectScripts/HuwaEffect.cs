5ed4f37fe6aad48464721d0d5aa7a2e6f015f2b2
KBC12006094\kbc12006094 <yuuya-kuniyoshi-20h@stu.kbc.ac.jp>
2022-05-18 10:43:07 +0900

    ふわふわ

--- CTeam/Assets/yuya/GionScripts/EffectScripts/HuwaEffect.cs
+++ CTeam/Assets/yuya/GionScripts/EffectScripts/HuwaEffect.cs
@@ -32,20 +32,21 @@ public class HuwaEffect : MonoBehaviour
 
         ray = new Ray(transform.position, -transform.up);
 
-        if (getbyunflag == false)
-        {
-            if (rb.velocity.y < -2.8f)
-            {
-                childObjhuwa.SetActive(false);
-            }
 
-            if (Physics.Raycast(ray, out hit, 1.0f))
-            {
-                childObjhuwa.SetActive(false);
-                //Debug.Log(hit.collider.gameObject.transform.position);
-                Debug.DrawRay(ray.origin, ray.direction, Color.red, 3.0f);
-            }
-            else
+        if (rb.velocity.y < -2.8f)
+        {
+            childObjhuwa.SetActive(false);
+        }
+
+        if (Physics.Raycast(ray, out hit, 1.0f))
+        {
+            childObjhuwa.SetActive(false);
+            //Debug.Log(hit.collider.gameObject.transform.position);
+            Debug.DrawRay(ray.origin, ray.direction, Color.red, 3.0f);
+        }
+        else
+        {
+            if (getbyunflag == false)
             {
                 childObjhuwa.SetActive(true);
             }
