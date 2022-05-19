7cdad7e0e30f1dd5495ed3589abed470673116cd
KBC12006118\kbc12006118 <koudai-arashiro-20h@stu.kbc.ac.jp>
2022-05-19 10:03:34 +0900

    テスト

---
+++ CTeam/Assets/Ara/Script/CopyGionChangeAra.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CopyGionChangeAra : MonoBehaviour
+{
+    public Text changetext;     //切り替えによって擬音が変わるテキストを入れる用変数
+
+    int getnumber;              // Gion スクリプトから number を受け取る用変数
+
+
+    void Update()
+    {
+        getnumber = TuAra.ChangeNumber();        // Gion スクリプトから number を受け取る
+
+        switch (getnumber)
+        {
+            case 0:
+                changetext.text = "すべすべ";
+                break;
+
+            case 1:
+                changetext.text = "ふわふわ";
+                break;
+
+            case 2:
+                changetext.text = "すけすけ";
+                break;
+
+            case 3:
+                changetext.text = "びゅんびゅん";
+                break;
+
+            case 4:
+                changetext.text = "ばらばら";
+                break;
+
+            case 5:
+                changetext.text = "ねばねば";
+                break;
+
+        }
+    }
+}
